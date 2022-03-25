8cf05dcc457bbcb21d5670d56c0b90bf03384e21
alezzle <niekras@tut.by>
2022-03-25 12:19:28 +0300

    dev

--- Source/YourProjectName.dpr
+++ Source/YourProjectName.dpr
@@ -3,7 +3,9 @@ program YourProjectName;
 uses
   Vcl.Forms,
   cController in 'cController.pas',
-  vMain in 'vMain.pas' {ViewMain};
+  vMain in 'vMain.pas' {ViewMain},
+  Apollo_MVC_VCL in '..\Vendors\Apollo_MVC_VCL\Apollo_MVC_VCL.pas' {ViewVCLBase},
+  Apollo_MVC_Core in '..\Vendors\Apollo_MVC_Core\Apollo_MVC_Core.pas';
 
 {$R *.res}
 
